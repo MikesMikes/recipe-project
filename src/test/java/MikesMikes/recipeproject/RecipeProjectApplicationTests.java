d7ffeae78eb912664fe6104f9696ec53b0d26edb
MikesMikes <michaelkaliber15@gmail.com>
2022-03-03 17:33:56 +0000

    initial commit

---
+++ src/test/java/MikesMikes/recipeproject/RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package MikesMikes.recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
